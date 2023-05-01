6a0fc7c5c169c4ac445145b9b75adc637bae5071
Firebreak-2 <xxibahimgamerxx123123@gmail.com>
2023-05-01 10:05:20 +0400

    Seperate CLI project to two Host and Client projects

---
+++ OnlineStore.Client.CLI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using AdvancedCommands;
+using OnlineStore.Core;
+
+namespace OnlineStore.CLI;
+
+public static class Program
+{
+    public static CommandRunner Runner = new();
+    public static void Main(string[] args)
+    {
+        DoClient();
+    }
+
+    public static void DoClient()
+    { 
+        Task.Run(async () => await Store.Initialize(false, 1030)).GetAwaiter().GetResult();
+    }
+}
---
+++ OnlineStore.Host.CLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using AdvancedCommands;
+using OnlineStore.Core;
+
+namespace OnlineStore.CLI;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        DoHost();
+    }
+
+    public static void DoHost()
+    { 
+        Task.Run(async () => await Store.Initialize(true, 1030)).GetAwaiter().GetResult();
+    }
+}
